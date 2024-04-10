6830b0e2f31aabfe28867b2fb6386ddc9f3f6b13
guihuawen <guihuawen@didiglobal.com>
2024-04-10 16:31:15 +0800

    test

--- gluten-core/src/main/scala/org/apache/gluten/execution/RDDScanExecTransformer.scala
+++ gluten-core/src/main/scala/org/apache/gluten/execution/RDDScanExecTransformer.scala
@@ -1,20 +1,17 @@
 package org.apache.gluten.execution
 
-import org.apache.gluten.substrait.rel.LocalFilesNode
+
 import org.apache.spark.rdd.RDD
 import org.apache.spark.sql.catalyst.InternalRow
 import org.apache.spark.sql.catalyst.expressions.Attribute
 import org.apache.spark.sql.catalyst.util.truncatedString
-import org.apache.spark.sql.catalyst.plans.physical.{Partitioning, UnknownPartitioning}
-import org.apache.spark.sql.connector.read.InputPartition
 import org.apache.spark.sql.execution.RDDScanExec
 
 
 class RDDScanExecTransformer(output: Seq[Attribute],
      rdd: RDD[InternalRow],
      name: String)
-  extends RDDScanExec(output, rdd, name)
-    with BasicScanExecTransformer {
+  extends RDDScanExec(output, rdd, name) {
 
   private def rddName: String = Option(rdd.name).map(n => s" $n").getOrElse("")
 
@@ -23,11 +20,6 @@ class RDDScanExecTransformer(output: Seq[Attribute],
   override def simpleString(maxFields: Int): String = {
     s"$nodeName${truncatedString(output, "[", ",", "]", maxFields)}"
   }
-
-  /** This can be used to report FileFormat for a file based scan operator. */
-  override val fileFormat: LocalFilesNode.ReadFileFormat = _
-
-  override def getPartitions: Seq[InputPartition] = partitions
 
 
 
